69ca9e7146cb4bf9b673b4cb4262b77e74e0f05c
Balázs Gerlei <gerlotdev@gmail.com>
2018-10-18 20:55:01 +0200

    Changed to using TextInputLayout and TextInputEditText instead of a regular EditText. Minimum 2 characters required for search keywords so results will be fewer. Implemented firing a new API request for each items in the search result to get the details for the movie and be able to display the budget (which is not returned in the search Api) via RxJava operators in GetMoviesUseCase.

--- MovieDBViewer/app/src/main/java/hu/gerlotdev/moviedbviewer/domain/usecase/GetMoviesUseCase.java
+++ MovieDBViewer/app/src/main/java/hu/gerlotdev/moviedbviewer/domain/usecase/GetMoviesUseCase.java
@@ -1,15 +1,21 @@
 package hu.gerlotdev.moviedbviewer.domain.usecase;
 
+import java.util.List;
+
 import javax.inject.Inject;
 
+import hu.gerlotdev.moviedbviewer.data.model.Movie;
 import hu.gerlotdev.moviedbviewer.data.model.MoviePage;
 import hu.gerlotdev.moviedbviewer.data.network.MovieApi;
 import hu.gerlotdev.moviedbviewer.data.network.NetworkManager;
 import hu.gerlotdev.moviedbviewer.domain.executor.PostExecutionThread;
 import hu.gerlotdev.moviedbviewer.presentation.MovieDBViewerApplication;
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
 import io.reactivex.Single;
+import io.reactivex.functions.Function;
 
-public class GetMoviesUseCase extends UseCase<MoviePage> {
+public class GetMoviesUseCase extends UseCase<List<Movie>> {
 
     @Inject
     MovieApi movieApi;
@@ -26,8 +32,26 @@ public class GetMoviesUseCase extends UseCase<MoviePage> {
     }
 
     @Override
-    protected Single<MoviePage> buildUseCaseObservable() {
-        return movieApi.getMovies(NetworkManager.API_KEY, query);
+    protected Single<List<Movie>> buildUseCaseObservable() {
+        return movieApi.getMovies(NetworkManager.API_KEY, query)
+                .map(new Function<MoviePage, List<Movie>>() {
+                    @Override
+                    public List<Movie> apply(MoviePage moviePage) throws Exception {
+                        return moviePage.getResults();
+                    }
+                })
+                .toObservable().flatMap(new Function<List<Movie>, ObservableSource<Movie>>() {
+                    @Override
+                    public ObservableSource<Movie> apply(List<Movie> movies) throws Exception {
+                        return Observable.fromIterable(movies);
+                    }
+                })
+                .flatMap(new Function<Movie, ObservableSource<? extends Movie>>() {
+                    @Override
+                    public ObservableSource<? extends Movie> apply(Movie movie) throws Exception {
+                        return movieApi.getMovie(movie.getId(), NetworkManager.API_KEY).toObservable();
+                    }
+                }).toList();
     }
 
 }
--- MovieDBViewer/app/src/main/java/hu/gerlotdev/moviedbviewer/presentation/main/MainFragment.java
+++ MovieDBViewer/app/src/main/java/hu/gerlotdev/moviedbviewer/presentation/main/MainFragment.java
@@ -5,6 +5,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
@@ -19,9 +21,12 @@ import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 
+import java.util.List;
+
 import javax.inject.Inject;
 
 import hu.gerlotdev.moviedbviewer.R;
+import hu.gerlotdev.moviedbviewer.data.model.Movie;
 import hu.gerlotdev.moviedbviewer.data.model.MoviePage;
 import hu.gerlotdev.moviedbviewer.data.network.MovieApi;
 import hu.gerlotdev.moviedbviewer.data.network.NetworkManager;
@@ -49,7 +54,8 @@ public class MainFragment extends BaseFragment<MainFragment.MainFragmentListener
     GetMoviesUseCase getMoviesUseCase;
 
     private Toolbar toolbar;
-    private EditText etSearch;
+    private TextInputLayout tilSearch;
+    private TextInputEditText etSearch;
     private Button btnSearch;
     private ViewFlipper vfThreeState;
     private TextView tvError;
@@ -92,6 +98,7 @@ public class MainFragment extends BaseFragment<MainFragment.MainFragmentListener
             listener.setToolbarAsSupportActionBar(toolbar);
         }
 
+        tilSearch = view.findViewById(R.id.tilSearch);
         etSearch = view.findViewById(R.id.etSearch);
         btnSearch = view.findViewById(R.id.btnSearch);
         vfThreeState = view.findViewById(R.id.vfThreeState);
@@ -110,26 +117,31 @@ public class MainFragment extends BaseFragment<MainFragment.MainFragmentListener
             public void onClick(View v) {
                 if (etSearch.getText() != null
                         && etSearch.getText().toString() != null
-                        && !etSearch.getText().toString().isEmpty()
                         && getMoviesUseCase != null) {
-                    // Hide the keyboard
-                    InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
-                    imm.hideSoftInputFromWindow(etSearch.getWindowToken(), 0);
+                    if (etSearch.getText().toString().length() < 2) {
+                        tilSearch.setError(getResources().getString(R.string.at_least_two));
+                    } else {
+                        tilSearch.setError(null);
 
-                    vfThreeState.setDisplayedChild(LOADING_VIEW);
-                    getMoviesUseCase.setQuery(etSearch.getText().toString());
-                    getMoviesUseCase.execute(new GetMoviesObserver());
+                        // Hide the keyboard
+                        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Activity.INPUT_METHOD_SERVICE);
+                        imm.hideSoftInputFromWindow(etSearch.getWindowToken(), 0);
+
+                        vfThreeState.setDisplayedChild(LOADING_VIEW);
+                        getMoviesUseCase.setQuery(etSearch.getText().toString());
+                        getMoviesUseCase.execute(new GetMoviesObserver());
+                    }
                 }
             }
         });
     }
 
-    private final class GetMoviesObserver extends DisposableSingleObserver<MoviePage> {
+    private final class GetMoviesObserver extends DisposableSingleObserver<List<Movie>> {
 
         @Override
-        public void onSuccess(MoviePage moviePage) {
-            if (moviePage.getResults() != null) {
-                rvMoviesListAdapter.setMovies(moviePage.getResults());
+        public void onSuccess(List<Movie> moviePage) {
+            if (moviePage != null) {
+                rvMoviesListAdapter.setMovies(moviePage);
                 vfThreeState.setDisplayedChild(CONTENT_VIEW);
             }
         }
